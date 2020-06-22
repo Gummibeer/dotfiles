d4576f4b4bed5a4428320173a562be2ae38a5972
Tom Witkowski <dev.gummibeer@gmail.com>
2020-06-22 16:15:01 +0200

    configure default pull behavior

--- install.sh
+++ install.sh
@@ -26,7 +26,8 @@ composer global require bamarni/composer-bin-plugin
 [ ! -f ~/.global.gitignore ] && ln -s ~/.dotfiles/stubs/.global.gitignore ~/.global.gitignore
 git config --global core.autocrlf input
 git config --global core.excludesfile ~/.global.gitignore
-git config --global pull.ff only
+git config --global pull.rebase false 
+git config --global pull.ff false
 git config --global commit.gpgsign true
 git config --global user.name "Tom Witkowski"
 git config --global user.email "dev.gummibeer@gmail.com"
